4128e7231eb225bdf21bc60ca5331efe655c903b
hyun <hyun@smgui-MacBook-Pro.local>
2017-08-07 12:12:02 +0900

    2차로그인 회윈가입 파일

---
+++ hyun/plot_login/src/routes.js
@@ -0,0 +1,9 @@
+import Strat from './components/Start.vue';
+import User from './components/User.vue';
+
+
+export const routes = [
+  { path: '/', component: Start },
+  { path: '/user', component: User }
+
+];
