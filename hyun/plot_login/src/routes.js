b71ef3bfc2f8626150ce63b3afb91d07c870180d
hyun <hyun@smgui-MacBook-Pro.local>
2017-08-07 12:40:57 +0900

    plot_login

--- hyun/plot_login/src/routes.js
+++
@@ -1,9 +0,0 @@
-import Strat from './components/Start.vue';
-import User from './components/User.vue';
-
-
-export const routes = [
-  { path: '/', component: Start },
-  { path: '/user', component: User }
-
-];
